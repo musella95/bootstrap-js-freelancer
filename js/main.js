2b110a79d4c8aff05646eb8889a76929776a09b3
musella95 <musellamatteoo@hotmail.com>
2023-11-27 11:44:38 +0100

    start array of discount

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 function serviceBook(event){
     event.preventDefault();
-    let typeOfWork = document.getElementById("type-of-work");
-    let optionOfWork  = typeOfWork.options[typeOfWork.selectedIndex].value;
-     console.log(optionOfWork)
+    let typeOfWork = document.getElementById("type-of-work").value;
 
-     if
+    let hours = document.getElementById("hours").value;
+    hours = parseInt(hours);
+
+    let discount =["YHDNU32","JANJC63","PWKCN25","SJDPO96","POCIE24",]
+    let price;
+    
+    
+    switch(typeOfWork){
+        case "backEnd":
+            price = hours * 20.50;
+            break;
+        case "frontEnd":
+            price = hours * 15,30;
+            break;
+        case "projectAnalysis":
+            price = hours * 33,60;
+            break;
+            default:
+                price = alert("CAMPO NON COMPILATO");
+
+
+
+    }
+    let totalCost = price;
+    
 }
